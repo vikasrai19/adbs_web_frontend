28c040fd385e9a6102e739b73daf3cc504ee5618
sharan kumar <sharanperla1234@gmail.com>
2023-11-01 08:49:15 +0530

    added routes using react router dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Signup from './pages/Signup'
+import Splash from './pages/Splash'
+import Login from './pages/Login'
 
 function App() {
   return (
-    <div>
-      hi
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/signup' element={<Signup/>}/> 
+        <Route path='/Splash' element={<Splash/>}/> 
+        <Route path='/Login' element={<Login/>}/> 
+      </Routes>
+    </BrowserRouter>
   )
 }
 
