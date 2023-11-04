7794299cede43776d7f4fd5a0bcbba0e3b3ba1b4
sharan kumar <sharanperla1234@gmail.com>
2023-11-04 23:35:06 +0530

    home page ui desing 30

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,56 @@
 import React from 'react'
+import { } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Home() {
   return (
-    <div>
-      home
+    <div className='min-h-screen flex'>
+      <div className='hidden primary h-screen w-[15vw] md:p-5 md:flex flex-col justify-normal gap-10'>
+        <div>
+
+          <div className=' py-5'>
+            Buswatch
+          </div>
+          <div className='flex items-center gap-4'>
+            <div className='w-14 h-14 rounded-xl object-fill overflow-hidden outline outline-slate-50'> <img src="https://www.diethelmtravel.com/wp-content/uploads/2016/04/bill-gates-wealthiest-person.jpg" alt="icon" /></div>
+            <div>
+              <h1 className='text-black text-2xl font-semibold'>Sharan</h1>
+              <h1 className='text-black text-md opacity-50 font-semibold'>student</h1>
+            </div>
+            <img src="/icons/editprofile.png" alt="" />
+          </div>
+        </div>
+        <div className='flex flex-col gap-3'>
+          <div className='flex items-center gap-5'>
+            <img src="/icons/home.svg" alt="" />
+            <h1>home</h1>
+          </div>
+          <div className='flex items-center gap-5'>
+            <img src="/icons/payment.svg" alt="" />
+            <h1>home</h1>
+          </div>
+          <div className='flex items-center gap-5'>
+            <img src="/icons/contact.svg" alt="" />
+            <h1>home</h1>
+          </div>
+        </div>
+      </div>
+      <div className='w-full md:w-[85vw]'>
+        <div className='w-full bg-white h-10'>
+          <div className='flex justify-between md:justify-end  p-3 md:p-5 space-x-2'>
+            <div className='float-left md:hidden'>
+            <img src="/icons/menu.svg" alt="" />
+
+            </div>
+            <div className='flex gap-2'>
+            <img src="/icons/emai-1.svg" alt="" />
+            <img src="/icons/settings.svg" alt="" />
+
+            </div>
+          </div>
+        </div>
+
+      </div>
     </div>
   )
 }
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,45 +1,79 @@
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 
 function Signup() {
-  const navigate=useNavigate();
-  const handleSIgnup=()=>{
+  const [type, setType] = useState(null);
+  const [draw, setDraw] = useState(0);
+  const navigate = useNavigate();
+  const handleSIgnup = () => {
+    if (type == null) {
+      setDraw(1)
+    }
+    else {
+      navigate('/login')
+    }
+  }
+  const moveToLogin = () => {
     navigate('/login')
   }
-  const moveToLogin=()=>{
-     navigate('/login')
+  const studentClick = () => {
+    setType('student')
+    setDraw(0)
   }
-   return (
-     <div className='min-h-screen flex flex-col justify-between items-center px-3 py-[20px]'>
-       <div className='w-full'>
-         <FontAwesomeIcon icon={faArrowLeft} size="lg" className='float-left' />
-       </div>
-       <div className='flex flex-col justify-center items-center space-y-3 w-full'>
-         <div className='flex flex-col -space-y-3 items-center justify-center'>
-           <h2 className='text-[1rem] font-normal'>create to your</h2>
-           <h1 className='text-[3rem] font-bold'>Bus Watch</h1>
-           <h2 className='text-[1rem]'>Account</h2>
- 
-         </div>
-         <div className='flex items-center justify-center flex-col w-full space-y-3'>
-           <input type="text" className='bg-inputbg w-full h-[57px] md:w-1/4 rounded-[10px] placeholder:text-center px-3' placeholder='student name' />
-           <input type="text" className='bg-inputbg w-full h-[57px] md:w-1/4 rounded-[10px] placeholder:text-center px-3' placeholder='usn' />
-           <input type="text" className='bg-inputbg w-full h-[57px] md:w-1/4 rounded-[10px] placeholder:text-center px-3' placeholder='registered mobile' />
-           <input type="text" className='bg-inputbg w-full h-[57px] md:w-1/4 rounded-[10px] placeholder:text-center px-3' placeholder='bus number' />
-           <input type="password" className='bg-inputbg w-full h-[57px] md:w-1/4  rounded-[10px] placeholder:text-center px-3' placeholder='create password' />
-           <input type="password" className='bg-inputbg w-full h-[57px] md:w-1/4  rounded-[10px] placeholder:text-center px-3' placeholder=' congirm password' />
-           <input type="button" value="Signup" className='text-btnw hite bg-tblue w-[189px] h-[52px] rounded-[15px] text-btnwhite cursor-pointer' onClick={handleSIgnup}/>
-         </div>
-       </div>
-       <div className='cursor-pointer' onClick={moveToLogin} >
-         <p>Already have an account <span className='text-tblue'>Login</span></p>
-       </div>
-     </div>
- 
-   )
- 
+  const driverClick = () => {
+    setType('driver')
+    setDraw(0)
+  }
+  const collegClick = () => {
+    setType('college')
+    setDraw(0)
+  }
+  return (
+    <div className='min-h-screen flex flex-col justify-between items-center px-3 py-[20px]'>
+      <div className='w-full'>
+        <FontAwesomeIcon icon={faArrowLeft} size="lg" className='float-left' />
+      </div>
+      <div className='flex flex-col justify-center items-center space-y-3 w-full'>
+        <div className='flex flex-col -space-y-3 items-center justify-center'>
+          <h2 className='text-[1rem] font-normal'>create to your</h2>
+          <h1 className='text-[3rem] font-bold'>Bus Watch</h1>
+          <h2 className='text-[1rem]'>Account</h2>
+
+        </div>
+        <div className='flex items-center justify-center flex-col w-full space-y-3'>
+          {type != null ? <input type="text" className='bg-inputbg w-full h-[57px] md:w-1/4 rounded-[10px] placeholder:text-center px-3' placeholder={type} /> : ''}
+          <input type="text" className='bg-inputbg w-full h-[57px] md:w-1/4 rounded-[10px] placeholder:text-center px-3' placeholder='student name' />
+          <input type="text" className='bg-inputbg w-full h-[57px] md:w-1/4 rounded-[10px] placeholder:text-center px-3' placeholder='usn' />
+          <input type="text" className='bg-inputbg w-full h-[57px] md:w-1/4 rounded-[10px] placeholder:text-center px-3' placeholder='registered mobile' />
+          <input type="text" className='bg-inputbg w-full h-[57px] md:w-1/4 rounded-[10px] placeholder:text-center px-3' placeholder='bus number' />
+          <input type="password" className='bg-inputbg w-full h-[57px] md:w-1/4  rounded-[10px] placeholder:text-center px-3' placeholder='create password' />
+          <input type="password" className='bg-inputbg w-full h-[57px] md:w-1/4  rounded-[10px] placeholder:text-center px-3' placeholder=' congirm password' />
+          <input type="button" value="Signup" className=' bg-tblue w-[189px] h-[52px] rounded-[15px] text-btnwhite cursor-pointer' onClick={handleSIgnup} />
+        </div>
+      </div>
+      <div className='cursor-pointer' onClick={moveToLogin} >
+        <p>Already have an account <span className='text-tblue'>Login</span></p>
+      </div>
+      <div className={`${draw == 0 ? 'hidden' : 'flex w-full absolute  flex-col justify-end bottom-0 items-center animation'}`}>
+
+        <div className='min-h-[50vh]  w-full flex items-center justify-center flex-col bg-white rounded-3xl border border-black'>
+
+          <div className=''>
+            <h1>Select account type</h1>
+          </div>
+          <div className='flex flex-col'>
+            <button className='text-lg font-medium h-[3rem] w-[21rem]  md:w-[20.5rem] md:h-[46px] lg:w-[22.5rem] lg:h-[56px] mt-3 text-btnwhite bg-tblue rounded-[10px]' onClick={studentClick}>Student</button>
+            <button className='text-lg font-medium h-[3rem] w-[21rem]  md:w-[20.5rem] md:h-[46px] lg:w-[22.5rem] lg:h-[56px] mt-3 text-btnwhite bg-tblue rounded-[10px]' onClick={driverClick}>Driver</button>
+            <button className='text-lg font-medium h-[3rem] w-[21rem]  md:w-[20.5rem] md:h-[46px] lg:w-[22.5rem] lg:h-[56px] mt-3 text-btnwhite bg-tblue rounded-[10px]' onclick={collegClick} >College</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  )
+
 }
 
 export default Signup
